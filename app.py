936be8c1549da39c15101c31831ead98ad5be3d9
ARSAM ZAMANI <arsamxman@gmail.com>
2025-10-31 18:10:29 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ import numpy as np
 
 with open('model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
-st.title("مدل ارزیابی ماشین ها")
+st.title("مدل ارزیابی ماشین ها نسخه ی فارسی")
 st.write("لطفا اطلاعات ماشین خود را وارد کنید")
 
-dict_buy={'low':0,'med':1,'high':2,'vhigh':3}
+dict_buy={'کم':0,'متوسط':1,'زیاد':2,'خیلی زیاد':3}
 buy=st.radio("میزان قیمت ماشین",dict_buy.keys())
 
-dict_maint={'low':0,'med':1,'high':2,'vhigh':3}
+dict_maint={'کم':0,'متوسط':1,'زیاد':2,'خیلی زیاد':3}
 maint=st.radio("میزان قیمت نگهداری ماشین",dict_maint.keys())
 
-dict_doors={'2':0,'3':1,'4':2,'more5':3}
+dict_doors={'۲':0,'۳':1,'۴':2,'بیشتر از ۵':3}
 doors=st.radio("تعداد در های ماشین",dict_doors.keys())
 
-dict_persons={'2':0,'4':1,'more':2}
+dict_persons={'۲':0,'۴':1,'بیشتراز۴':2}
 persons=st.radio("ظرفیت ماشین",dict_persons.keys())
 
-dict_lug_boot={'small':0,'med':1,'big':2}
+dict_lug_boot={'کوچک':0,'متوسط':1,'بزرگ':2}
 lug_boot=st.radio("سایز صندوق عقب ماشین",dict_lug_boot.keys())
 
-dict_safety={'low':0,'med':1,'high':2}
+dict_safety={'کم':0,'متوسط':1,'زیاد':2}
 safety=st.radio("امنیت  ماشین",dict_safety.keys())
 
 
